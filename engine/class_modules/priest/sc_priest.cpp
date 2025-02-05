372e4c2db7b65fc39178021ce74c4ca1e692668b
Saeldur <5496498+Saeldur@users.noreply.github.com>
2025-02-05 01:29:01 +0000

    [Shadow] Bugs Bugs Bugs Bugs Bugs * God I love bugs

--- engine/class_modules/priest/sc_priest.cpp
+++ engine/class_modules/priest/sc_priest.cpp
@@ -1180,7 +1180,21 @@ struct power_infusion_t final : public priest_spell_t
     // Trigger PI on the actor only if casting on itself
     if ( priest().options.self_power_infusion || priest().talents.twins_of_the_sun_priestess.enabled() )
     {
-      player->buffs.power_infusion->trigger( 1, power_infusion_magnitude );
+      bool tww2_4pc      = priest().sets->has_set_bonus( PRIEST_SHADOW, TWW2, B4 );
+      bool twins_4pc_bug = player->bugs && tww2_4pc && priest().talents.twins_of_the_sun_priestess.enabled();
+      if ( tww2_4pc && player->buffs.power_infusion->check() )
+      {
+        timespan_t extend_amount = std::min( ( twins_4pc_bug ? 2 : 1 ) * player->buffs.power_infusion->buff_duration(),
+                                             30_s - player->buffs.power_infusion->remains() );
+        if ( extend_amount > 0_s )
+          player->buffs.power_infusion->extend_duration( player, extend_amount );
+      }
+      else
+      {
+        player->buffs.power_infusion->trigger(
+            1, power_infusion_magnitude, -1,
+            ( twins_4pc_bug ? 2 : 1 ) * player->buffs.power_infusion->buff_duration() );
+      }
     }
   }
 };
--- engine/class_modules/priest/sc_priest_shadow.cpp
+++ engine/class_modules/priest/sc_priest_shadow.cpp
@@ -1223,12 +1223,28 @@ struct void_bolt_base_t : public priest_spell_t
 
   void_bolt_extension_t* void_bolt_extension;
   bool trigger_shadowy_apparitions;
+  bool only_cwc;
+  const spell_data_t* mind_flay_spell;
+  const spell_data_t* mind_flay_insanity_spell;
+  const spell_data_t* void_torrent_spell;
 
   void_bolt_base_t( priest_t& p, util::string_view name, util::string_view options )
-    : priest_spell_t( name, p, p.specs.void_bolt ), void_bolt_extension( nullptr ), trigger_shadowy_apparitions( true )
+    : priest_spell_t( name, p, p.specs.void_bolt ),
+      void_bolt_extension( nullptr ),
+      trigger_shadowy_apparitions( true ),
+      only_cwc( false ),
+      mind_flay_spell( p.specs.mind_flay ),
+      mind_flay_insanity_spell( p.talents.shadow.mind_flay_insanity_spell ),
+      void_torrent_spell( p.talents.shadow.void_torrent )
   {
+    add_option( opt_bool( "only_cwc", only_cwc ) );
     parse_options( options );
-    use_off_gcd                = true;
+
+    if ( !p.bugs || !p.is_ptr() )
+      only_cwc = false;
+
+    usable_while_casting = use_while_casting = only_cwc;
+
     energize_type              = action_energize::ON_CAST;
     cooldown->hasted           = true;
     affected_by_shadow_weaving = true;
@@ -1244,6 +1260,19 @@ struct void_bolt_base_t : public priest_spell_t
   {
     if ( !priest().buffs.voidform->check() )
     {
+      return false;
+    }
+
+    if ( p().is_ptr() && only_cwc )
+    {
+      if ( player->channeling == nullptr )
+        return false;
+
+      if ( player->channeling->data().id() == mind_flay_spell->id() ||
+           player->channeling->data().id() == mind_flay_insanity_spell->id() ||
+           player->channeling->data().id() == void_torrent_spell->id() )
+        return priest_spell_t::ready();
+
       return false;
     }
 
